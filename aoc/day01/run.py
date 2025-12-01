f14d74d2bb0b4819c85cc1aa4c3a8739cde1daf3
Jonathan Camp <jonathan.camp@intelecy.com>
2025-12-01 07:30:01 +0100

    day 01

---
+++ aoc/day01/run.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env -S uv run --script
+
+import os
+import random
+import sys
+
+import numpy as np
+from typing_extensions import Dict
+
+part1 = 0
+part2 = 0
+
+src = open("input.txt", "r").readlines()
+
+example = """
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+""".splitlines()
+
+# src = example
+
+src = [r.strip() for r in src if r.strip()]
+
+dial = 50
+
+for line in src:
+    d, n = line[0], line[1:]
+
+    for s in range(int(n)):
+        if d == "L":
+            dial -= 1
+        elif d == "R":
+            dial += 1
+
+        dial %= 100
+
+        if dial == 0:
+            part2 += 1
+
+    if dial == 0:
+        part1 += 1
+
+print("part1:", part1)
+print("part2:", part2)
